c88e6cbd89d2c9d16ad770d79590f3ddc84c3521
sattapaal <sattapaal@gmail.com>
2013-10-17 15:39:10 +0000

    Master Commit
    First Commit to Git Hub

---
+++ MyDotNetMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyDotNetMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "I see. So this is inherited from the ViewBag object.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Killer()
+        {
+            ViewBag.Message = "This is killer innit.";
+
+            return View();
+        }
+
+    }
+}
